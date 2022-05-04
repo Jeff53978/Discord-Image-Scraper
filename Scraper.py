b2e0a4d27643fa068ced202c2c916ec3d3c2eb1a
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-05-05 00:39:02 +0200

    Added Files

---
+++ Scraper.py
@@ -0,0 +1,25 @@
+import discord, os, random, json
+
+client = discord.Bot()
+try: os.system("cls")
+except: os.system("clear")
+
+@client.event
+async def on_ready():
+    print(f"Logged in as: {client.user.name}")
+    print("Loading Guilds... \n")
+    for guild in client.guilds:
+        print(f"Loaded - {guild.name}")
+    print("")
+    for guild in client.guilds:
+        path = random.randint(1000000, 9000000)
+        for channel in guild.text_channels:
+            async for message in channel.history(limit=10000):
+                if message.attachments:
+                    for attachment in message.attachments:
+                        if os.path.exists(f"{os.getcwd()}\\{path}"): pass
+                        else: os.mkdir(f"{os.getcwd()}\\{path}"); pass
+                        await attachment.save(f"{os.getcwd()}\\{path}\\{attachment.filename}")
+                        print(f"[*] Saved - Filename: {attachment.filename} Guild: {guild.name} Folder: {path}")
+
+client.run(json.loads(open("config.json", "r").read())["token"])
