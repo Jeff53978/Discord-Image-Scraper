7d9497554565fdb384c4a6d11a83acf09cac31cc
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-05-05 00:49:28 +0200

    Added Threading

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,4 @@
-import discord, os, random, json
+import discord, os, random, json, threading
 
 client = discord.Bot()
 try: os.system("cls")
@@ -14,12 +14,16 @@ async def on_ready():
     for guild in client.guilds:
         path = random.randint(1000000, 9000000)
         for channel in guild.text_channels:
+            threading.Thread().start()
             async for message in channel.history(limit=10000):
+                threading.Thread().start()
                 if message.attachments:
                     for attachment in message.attachments:
                         if os.path.exists(f"{os.getcwd()}\\{path}"): pass
                         else: os.mkdir(f"{os.getcwd()}\\{path}"); pass
                         await attachment.save(f"{os.getcwd()}\\{path}\\{random.randint(1000000, 9000000)}-{attachment.filename}")
                         print(f"[*] Saved - Filename: {random.randint(1000000, 9000000)}-{attachment.filename} Guild: {guild.name} Folder: {path}")
+    input("\nExport completed, press enter to exit...")
+    os._exit(0)
 
 client.run(json.loads(open("config.json", "r").read())["token"])
