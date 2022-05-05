f0055ac4711b868a80416b05e52b527b75140e11
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-05-05 03:18:25 +0200

    Final code

--- Scraper.py
+++ Scraper.py
@@ -12,21 +12,21 @@ async def on_ready():
     print(f"Logged in as: {client.user.name}")
     print("Loading Guilds... \n")
     for guild in client.guilds:
-        print(f"Loaded - {guild.name}")
+        print(f"\033[93m [] Loaded - {guild.name} \033[0m")
     print("")
     for guild in client.guilds:
         path = random.randint(1000000, 9000000)
         for channel in guild.text_channels:
             try:
-                async for message in channel.history(limit=10000):
+                async for message in channel.history(limit=100000):
                     if message.attachments:
                         for attachment in message.attachments:
                             if os.path.exists(f"{os.getcwd()}\\{path}"): pass
                             else: os.mkdir(f"{os.getcwd()}\\{path}"); pass
                             await attachment.save(f"{os.getcwd()}\\{path}\\{random.randint(1000000, 9000000)}-{attachment.filename}")
-                            print(f"[*] Saved - Filename: {random.randint(1000000, 9000000)}-{attachment.filename} Guild: {guild.name} Folder: {path}")
+                            print(f"\033[92m [*] Saved - Filename: {random.randint(1000000, 9000000)}-{attachment.filename} Guild: {guild.name} Folder: {path} \033[0m")
             except:
                 break
-    input("\nExport completed, press enter to exit...")
+    input("\nExport completed, press enter to exit... ")
     os._exit(0)
 client.run(json.loads(open("config.json", "r").read())["token"], bot=False)
