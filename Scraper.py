ad362ae6ea1fcf0db0818466af651f059f8dd0f4
Snikker#6211 <99515656+Snikkertjuh@users.noreply.github.com>
2022-05-05 02:45:47 +0200

    Added Selfbot support

--- Scraper.py
+++ Scraper.py
@@ -1,9 +1,9 @@
-import os, random, json, threading
+import os, random, json, threading, asyncio
 
 try: import discord
-except: os.system("pip install py-cord==2.0.0b5")
+except: os.system("pip install discord.py")
 
-client = discord.Bot()
+client = discord.Client(self_bot=True)
 try: os.system("cls")
 except: os.system("clear")
 
@@ -15,19 +15,18 @@ async def on_ready():
         print(f"Loaded - {guild.name}")
     print("")
     for guild in client.guilds:
-        threading.Thread().start()
         path = random.randint(1000000, 9000000)
         for channel in guild.text_channels:
-            threading.Thread().start()
-            async for message in channel.history(limit=10000):
-                threading.Thread().start()
-                if message.attachments:
-                    for attachment in message.attachments:
-                        if os.path.exists(f"{os.getcwd()}\\{path}"): pass
-                        else: os.mkdir(f"{os.getcwd()}\\{path}"); pass
-                        await attachment.save(f"{os.getcwd()}\\{path}\\{random.randint(1000000, 9000000)}-{attachment.filename}")
-                        print(f"[*] Saved - Filename: {random.randint(1000000, 9000000)}-{attachment.filename} Guild: {guild.name} Folder: {path}")
+            try:
+                async for message in channel.history(limit=10000):
+                    if message.attachments:
+                        for attachment in message.attachments:
+                            if os.path.exists(f"{os.getcwd()}\\{path}"): pass
+                            else: os.mkdir(f"{os.getcwd()}\\{path}"); pass
+                            await attachment.save(f"{os.getcwd()}\\{path}\\{random.randint(1000000, 9000000)}-{attachment.filename}")
+                            print(f"[*] Saved - Filename: {random.randint(1000000, 9000000)}-{attachment.filename} Guild: {guild.name} Folder: {path}")
+            except:
+                break
     input("\nExport completed, press enter to exit...")
     os._exit(0)
-
-client.run(json.loads(open("config.json", "r").read())["token"])
+client.run(json.loads(open("config.json", "r").read())["token"], bot=False)
